17e3dd1c896d776f3347245bcbc0bac0b0bd5691
hoyinchu <chu.ho@husky.neu.edu>
2017-09-19 12:31:24 -0400

    Added ComponentType enum, Direction enum, Added a lot of comments on how things work, added methods in block, added methods in component type.

---
+++ src/Block/BlockProp.java
@@ -0,0 +1,12 @@
+package Block;
+
+/**
+ * This class represents the proterties that a block has. A calculation / formula / equations
+ * should be stored here
+ */
+// Don't know what properties blocks have, leaving it blank right now.
+class BlockProp {
+
+
+
+}
---
+++ src/Block/Direction.java
@@ -0,0 +1,8 @@
+package Block;
+
+/**
+ * An enumeration representing directions
+ */
+public enum Direction {
+  TOP,RIGHT,BOTTOM,LEFT;
+}
